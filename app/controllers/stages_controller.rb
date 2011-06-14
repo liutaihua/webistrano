273e7eee8631f33f0b14dbb1d19fb2c1414d1d5b
liutaihua <defage@gmail.com>
2011-06-14 16:00:10 +0800

    hide some tasks of stages.

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -14,7 +14,7 @@ class StagesController < ApplicationController
   # GET /projects/1/stages/1.xml
   def show
     @stage = current_project.stages.find(params[:id])
-    @task_list = [['All tasks: ', '']] + @stage.list_tasks.first[:name].sort()
+    @task_list = [['All tasks: ', '']] + @stage.list_tasks.collect{|task| [task[:name], task[:name]]}.sort()
 
     respond_to do |format|
       format.html # show.rhtml
